8828a574acd80a97adbc607882149bd54bfaf416
Braj <brajkishore87@gmail.com>
2022-07-28 13:12:35 +0530

    adding db connection string dynamically to key vault secret.

--- main.tf
+++ main.tf
@@ -9,7 +9,9 @@ resource "azurerm_resource_group" "rg" {
 locals {
   secrets= concat(flatten([ for s in var.StorageAccounts : {
       name="${s.name}-AccessKey", value="${azurerm_storage_account.storage[s.name].primary_access_key}"}
-  ]),[
+  ]),flatten([for k in var.sql_databases :{
+    name="${k.name}-sql-connectionstring",value= "Server=tcp:${azurerm_mssql_server.sql.fully_qualified_domain_name},1433;Initial Catalog=${azurerm_mssql_database.db[k.name].name};Persist Security Info=False;User ID=${var.admin_username == null ? "sqladmin" : var.admin_username};Password=${random_password.rpassword.result};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"
+  }]),[
         {name="sql-password", value=random_password.rpassword.result},
         #{name="sql-connectionstring", value= "Server=tcp:${azurerm_mssql_server.sql.fully_qualified_domain_name},1433;Initial Catalog=${azurerm_mssql_database.db.name};Persist Security Info=False;User ID=${var.admin_username == null ? "sqladmin" : var.admin_username};Password=${random_password.rpassword.result};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"}
     ])
